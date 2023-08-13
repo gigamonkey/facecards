639ba8bc3210f08922170ecf4d48f0b529881acb
Peter Seibel <peter@gigamonkeys.com>
2023-08-13 09:40:04 -0700

    Plus bug fix for promoting row to deck plus general tidying.

--- public/js/study.js
+++ public/js/study.js
@@ -35,6 +35,7 @@ class State {
       [ a, b ] = [ b, a + b ];
     }
     this.rows.push(new Row(a));
+
     this.steps = this.deck.length * this.rows.length;
   }
 
@@ -65,39 +66,33 @@ class State {
 
   next() {
 
-    // 0. If there is only one row and it is not full, there is no next as
-    // we've moved everything into a sufficiently large row and are done.
+    // If we've emptied the deck and there is only one row left and it is not
+    // full, there is no next as we've moved everything into a sufficiently
+    // large row and are done.
 
-    if (this.deck.length === 0 && this.rows.length === 1 && !this.currentRow().isOverfull()) {
+    if (this.deck.length === 0 &&
+        this.rows.length === 1 &&
+        !this.currentRow().isOverfull())
+    {
       return null;
     }
 
-    // 1. We are in the middle of moving up, having just added a correct
-    // answer to some non-zero row. In that case, if that row is now overfull,
-    // we take the card from that row. Otherwise we revert to the default case
+    // We are either in the middle of moving up, having just added a correct
+    // answer to some non-zero row or we added an incorrect answer to the zeroth
+    // row and reset this.row.
     if (this.currentRow().isOverfull()) {
       return this.currentRow().next();
     }
 
-    // 2. Either we got one wrong and just added put a card back at the front
-    // of row zero or we promoted a card until a not-full row and therefore
-    // are back at zero.
-    //
-    // In either case where row is zero, if it is overfull, pop the next card
-    // from the row. If it is not and the if the deck is not empty, put a card
-    // from the deck into row zero and try again. If the deck is empty, remove
-    // row zero from rows and make it the deck and try again.
-
-    if (this.firstRow().isOverfull()) {
-      return this.currentRow().next();
+    // If we get here, it's because we are back at the zeroth row and it is not
+    // overfull. In that case we need to fill it from the deck. If the deck is
+    // empty we make the zeroth row the deck
+    if (this.deck.length > 0) {
+      this.firstRow().add(this.deck.pop());
     } else {
-      if (this.deck.length > 0) {
-        this.firstRow().add(this.deck.pop());
-      } else {
-        this.deck = this.rows.shift();
-      }
-      return this.next();
+      this.deck = this.rows.shift().cards;
     }
+    return this.next();
   }
 
   correct(card) {
@@ -122,9 +117,8 @@ let state = new State(cards);
 
 const next = () => {
   current = state.next();
-  //$('#progress').innerText = `${state.togo()} of ${state.steps}`;
   if (current === null) {
-    alert('done');
+    $('#current').innerText = 'Done!';
   } else {
     $('#current').replaceChildren(current);
   }
@@ -136,18 +130,20 @@ console.log(`Deck is ${state.deck.length} cards`);
 next();
 
 window.onkeydown = (e) => {
-  const back = current.querySelector('.back');
-  if (getComputedStyle(back).display === 'none') {
-    back.style.display = 'block';
-  } else {
-    if (e.code === 'ArrowLeft') {
-      state.incorrect(current);
-    } else if (e.code === 'ArrowRight') {
-      state.correct(current);
+  if (current) {
+    const back = current.querySelector('.back');
+    if (getComputedStyle(back).display === 'none') {
+      back.style.display = 'block';
     } else {
-      return; // ignore key; don't change anything
+      if (e.code === 'ArrowLeft') {
+        state.incorrect(current);
+      } else if (e.code === 'ArrowRight') {
+        state.correct(current);
+      } else {
+        return; // ignore key; don't change anything
+      }
+      back.style.display = 'none';
+      next();
     }
-    back.style.display = 'none';
-    next();
   }
 };

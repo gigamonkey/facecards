3194004172fb021838ed767b3626d2c3195f5884
Peter Seibel <peter@gigamonkeys.com>
2024-08-11 10:28:25 -0700

    Add flip cards feature on index page.

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,21 @@
 import { $, $$ } from './dom.js';
 
 $$('.card').forEach(card => {
+  card.dataset.showing = 'front';
+  const img = card.querySelector('img');
+  const back = card.querySelector('.back');
+  back.style.width = img.offsetWidth;
+
   card.onclick = () => {
-    const back = card.querySelector('.back');
-    if (getComputedStyle(back).display === 'none') {
-      back.style.display = 'block';
+    if (card.dataset.showing === 'front') {
+      img.style.display = 'none';
+      back.style.display = 'flex';
+      card.dataset.showing = 'back';
     } else {
+      img.style.display = 'block';
       back.style.display = 'none';
+      card.dataset.showing = 'front';
     }
-  }
+  };
+
 });

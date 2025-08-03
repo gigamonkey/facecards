da90ae1a6e5dfff1ca65bc035b310ef447bc7a68
Peter Seibel <peter@gigamonkeys.com>
2025-08-03 14:05:07 -0700

    Tidying.

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
+-- Student data exported from IC using "students for facecards" query.
 drop table if exists students;
-drop table if exists missing;
-
 .mode csv
 .import students-2025-26.csv students
 
+-- Find students for whom we have no photo in our photos directory.
+drop table if exists missing;
 create table missing (studentNumber text);
-
 .import '| ./find-missing.sh' missing

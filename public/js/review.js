7dccb06dbe642abab34343fb11d5cb46ed5db642
Peter Seibel <peter@gigamonkeys.com>
2025-08-05 08:18:02 -0700

    Incorparate touch events into review.js

--- public/js/review.js
+++ public/js/review.js
@@ -60,18 +60,54 @@ const next = () => {
   console.log(state.summary());
 }
 
+let touchStart = undefined;
+
+document.body.ontouchstart = (e) => {
+  touchStart = e.touches[0].clientX;
+};
+
+document.body.ontouchend = (e) => {
+  const touchEnd = e.changedTouches[0].clientX;
+  const d = Math.abs(touchEnd - touchStart);
+  if (d > 50) {
+    if (touchEnd > touchStart) {
+      handleEvent('right');
+    } else {
+      handleEvent('left');
+    }
+  } else {
+    handleEvent('none');
+  }
+};
+
+// Suppress scrolling.
+document.addEventListener('touchmove', (e) =>  { e.preventDefault(); }, { passive: false });
+
 window.onkeydown = (e) => {
+  let direction;
+  if (e.code === 'ArrowLeft') {
+    direction = 'left';
+  } else if (e.code === 'ArrowRight') {
+    direction = 'right';
+  } else {
+    direction = 'none';
+  }
+  handleEvent(direction);
+};
+
+
+const handleEvent = (direction) => {
   if (current) {
     const back = current.querySelector('.back');
     if (getComputedStyle(back).display === 'none') {
       back.style.display = 'block';
     } else {
-      if (e.code === 'ArrowLeft') {
+      if (direction === 'left') {
         state.incorrect(current);
-      } else if (e.code === 'ArrowRight') {
+      } else if (direction === 'right') {
         state.correct(current);
       } else {
-        return; // ignore key; don't change anything
+        return; // ignore; don't change anything
       }
       back.style.display = 'none';
       next();

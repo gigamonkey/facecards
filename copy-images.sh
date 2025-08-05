377296c5e4e2d265574163acbc8f60d81a6a1e45
Peter Seibel <peter@gigamonkeys.com>
2025-08-05 14:04:22 -0700

    print when copying.

--- copy-images.sh
+++ copy-images.sh
@@ -9,11 +9,12 @@ mkdir -p public/images
 sqlite3 db.db --tabs "select studentNumber from students" | while read -r n; do
     if [[ ! -e "public/images/$n.jpg" ]]; then
         if [[ -e "$photos/$n.jpg" ]]; then
+            echo "Copying $photos/$n.jpg"
             cp "$photos/$n.jpg" public/images/
         else
-            >&2 echo "Missing $n.jpg"
+            echo "Missing $n.jpg"
         fi
     else
-        >&2 echo "Already copied $n.jpg"
+        echo "Already copied $n.jpg"
     fi
 done

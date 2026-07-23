86365d39b7e8cb50dfeb197d74d6258dc48043c3
Peter Seibel <peter@gigamonkeys.com>
2026-07-22 22:33:52 -0600

    Config deployment stuff.

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// No need to export as all .gs files are loaded into the same namespace.
-// Managed by `hug config` — run e.g.
-//   hug config set SPREADSHEET_ID=<id> DRIVE_FOLDER_ID=<id>
+// No need to export as all .gs files are loaded into the same namespace
 const CONFIG = {
-  SPREADSHEET_ID: '',
-  DRIVE_FOLDER_ID: '',
+  SPREADSHEET_ID: '1CIdUWG9LEkuZXa8FtkvUElt6VQPQVYZo8yFyeo2sV1M',
+  DRIVE_FOLDER_ID: '1C3Qlyk8rAmyfzYYUF-xuCnSsDb_3foiH',
 };
